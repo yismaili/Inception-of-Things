25a47b1dc23d6b9b39fc374e2ce0d91b778791dc
younes ismaili <ismailiyounes20@gmail.com>
2023-12-21 21:31:59 +0100

    install K3s

--- p1/provision.sh
+++ p1/provision.sh
@@ -7,26 +7,22 @@ ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-# Update package  and upgrade list
+# Update package and upgrade list
 sudo apt-get update -y
-sudo apt upgrade -y
+sudo apt-get upgrade -y
 
-# innstall curl wget 
-sudo apt install -y curl wget
+# Install curl and wget
+sudo apt-get install -y curl wget
 
-# install k3s
+# Install K3s
+curl -sfL https://get.k3s.io | sh -
 
-curl -sfL https://get.k3s.io | sh –
-
+# Check K3s status
 sudo systemctl status k3s
 
 # Configure Kubectl
-
-mkdir ~/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && sudo chown $USER ~/.kube/config
-sudo chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
-
-
-
-
-
+mkdir -p ~/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown $USER ~/.kube/config
+sudo chmod 600 ~/.kube/config
+export KUBECONFIG=~/.kube/config
