f27427a699d858e5d57509edb6490e26950530bd
younes ismaili <ismailiyounes20@gmail.com>
2023-12-25 02:10:34 +0100

    change++

--- p2/scripts/deploy-app.sh
+++ p2/scripts/deploy-app.sh
@@ -8,14 +8,22 @@ sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 # Build Docker image
 sudo docker build -t localhost:5000/app1:v1 ./app1
+sudo docker build -t localhost:5000/app2:v1 ./app2
+sudo docker build -t localhost:5000/app3:v1 ./app3
 
 # Push Docker image to local registry
 sudo docker push localhost:5000/app1:v1
+sudo docker push localhost:5000/app2:v1
+sudo docker push localhost:5000/app3:v1
 cd ..
 
 # Apply Kubernetes configurations
 sudo kubectl apply -f app1-deployment.yaml
 sudo kubectl apply -f app1-service.yaml
+sudo kubectl apply -f app2-deployment.yaml
+sudo kubectl apply -f app2-service.yaml
+sudo kubectl apply -f app3-deployment.yaml
+sudo kubectl apply -f app3-service.yaml
 
 # Print a message
 echo "you good bro!!!!"
