8816804b75c472ab7ab157ebde5814a5315d3ecd
younes ismaili <ismailiyounes20@gmail.com>
2023-12-27 20:16:25 +0100

    fix all problem of ingress

--- p2/scripts/deploy-app.sh
+++ p2/scripts/deploy-app.sh
@@ -3,14 +3,13 @@
 # Exit script if any command fails
 set -e
 
-
 # Log in to Docker Hub
 sudo docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD" $DOCKER_REGISTRY
 
 # Log out from Docker Hub (optional)
 docker logout
 
-cd ../../vagrant
+# cd ../../vagrant
 
 # Check if Docker registry is already running
 if [ ! "$(sudo docker ps -q -f name=registry)" ]; then
@@ -18,7 +17,7 @@ if [ ! "$(sudo docker ps -q -f name=registry)" ]; then
     sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 fi
 # test in the host machine
-# cd ../confs 
+cd ../confs 
 # Build and push Docker images
 for app in app1 app2 app3; do
     echo "Building and pushing Docker image for $app"
