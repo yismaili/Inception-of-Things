181dfe1033236dde55f74b1bf2b99e9ad0fdd978
younes ismaili <ismailiyounes20@gmail.com>
2023-12-27 18:30:28 +0100

    fix all problem of ingress

--- p2/scripts/deploy-app.sh
+++ p2/scripts/deploy-app.sh
@@ -17,12 +17,13 @@ if [ ! "$(sudo docker ps -q -f name=registry)" ]; then
     # Start local Docker registry
     sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 fi
-
+# test in the host machine
+# cd ../confs 
 # Build and push Docker images
 for app in app1 app2 app3; do
     echo "Building and pushing Docker image for $app"
-    sudo docker build -t localhost:5000/$app:v3 ./$app
-    sudo docker push localhost:5000/$app:v3
+    sudo docker build -t localhost:5000/$app:v4 ./$app
+    sudo docker push localhost:5000/$app:v4
 done
 
 # Apply Kubernetes configurations
