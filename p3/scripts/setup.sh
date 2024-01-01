45df9f379495ef2092a2ddedc753559cd6068675
younes ismaili <ismailiyounes20@gmail.com>
2024-01-02 00:06:17 +0100

    start p3 install k3d and docker

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -7,8 +7,6 @@ sudo apt-get upgrade -y
 # Install curl and wget
 sudo apt-get install -y curl wget
 
-SERVER_IP="192.168.56.110"
-
 #install docker
 sudo apt install -y docker.io
 
@@ -17,10 +15,11 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 #install k3d using the script
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
-k3d --version
+sudo wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+sudo k3d --version
 
 # Create a Kubernetes cluster with k3d
-k3d cluster create master-cluster
+sudo k3d cluster create master-cluster
+
 
 
