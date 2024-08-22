24b28bd8e77a0975c2816d6997a5bbc032a8ab47
younes ismaili <ismailiyounes20@gmail.com>
2024-08-22 18:29:39 +0100

    Fix all problems in p1

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -2,15 +2,10 @@
 
 SERVER_IP="192.168.56.110"
 
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install -y curl
 
+# Install K3s Server
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip "$SERVER_IP"
 
-# Configure kubectl
-mkdir -p ~/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo chown $USER ~/.kube/config
-sudo chmod 600 ~/.kube/config
-
-export KUBECONFIG=~/.kube/config
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/k3s_token.txt
