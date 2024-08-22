28aaca470fce8974a0d429f461ed0f824655f914
younes ismaili <ismailiyounes20@gmail.com>
2024-08-22 19:07:41 +0100

    fix part 2

--- p2/scripts/setup-env.sh
+++ p2/scripts/setup-env.sh
@@ -3,20 +3,24 @@
 SERVER_IP="192.168.56.110"
 
 sudo apt-get update -y
-sudo apt-get install -y curl wget
+sudo apt-get install -y curl
 
 # Install k3s and set IP address
 sudo curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip "$SERVER_IP"
 
 # Install Docker
-sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |
+# sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# Install Docker
+sudo apt-get install -y docker.io
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
 
 # Check Docker status
 sudo systemctl status docker --no-pager
