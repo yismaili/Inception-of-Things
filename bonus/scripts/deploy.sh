21713e9a6b16461175e5b3cccbf749f48ba84847
Younes Ismaili <yismaili@e3r7p5.1337.ma>
2024-09-01 14:36:16 +0100

    fix part 3 && bonus

---
+++ bonus/scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex 
+sudo apt-get update -y
+k3d cluster create wil42
+
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+while [[ $(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.phase}") != "Running" ]]; do
+    echo "Waiting for argocd-server pod to be ready..."
+    sleep 10
+done
+
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d >> password.txt
+kubectl apply -f argocd-app.yaml
+# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl port-forward svc/argocd-server -n argocd 8080:443
